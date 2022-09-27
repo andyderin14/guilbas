2ad893a13c0bc9790262302477f60c92ac48923d
Andres Julian Derin <andresderin78@gmail.com>
2022-09-27 02:21:00 -0300

    Iniciando con la pagina de guilbas urban wear

---
+++ app.js
@@ -0,0 +1,29 @@
+const contenedor = document.getElementById('card-container');
+
+productos.forEach(producto => {
+          const div = document.createElement('div');
+          div.classList.add('card');
+          div.innerHTML += `
+          <div class="card-header">
+                    <img src="${producto.img}">
+          </div>
+          <div class="card-footer">
+                    <button class="card-btnComprar">Agregar al carrito<span class="material-symbols-outlined cart-btn">
+                    add_shopping_cart
+                    </span></button>
+                    <div class="card-desc">
+                              <h1>${producto.nombre}</h1>
+                              <h2>${producto.estacion}</h2>
+                              <h3>${producto.desc}</h3>
+                              <h4>$ ${producto.precio}</h4>
+                    </div>
+                    <div class="card-logo">
+                                        <img src="${producto.imgLogo}">
+                              <div class="card-logo-texto">
+                                        <span>${producto.slogan}</span>
+                              </div>
+                    </div>
+          </div>
+          `;
+          contenedor.appendChild(div);
+});
