c2bf286839f2af98107ba77c435babc636e98199
Andres Julian Derin <andresderin78@gmail.com>
2022-10-01 20:26:46 -0300

    Agregue btn vaciar y funcionaaaaaaa

--- modal.js
+++ modal.js
@@ -3,18 +3,25 @@ const abrirCarrito = document.getElementById('carrito-modal');
 const cerrarCarrito = document.getElementById('modal-cerrar');
 const modalCarrito = document.querySelector('.modal-carrito');
 
+const botonVaciarCarrito = document.getElementById('vaciarCarrito');
+
 abrirCarrito.addEventListener('click', () => {
-          modalContenedor.classList.toggle('modal-active')
+    modalContenedor.classList.toggle('modal-active')
 })
 
 cerrarCarrito.addEventListener('click', ()=> {
-          modalContenedor.classList.remove('modal-active')
-      })
-      
-      modalContenedor.addEventListener('click', () =>{
-          cerrarCarrito.click();
-      })
+    modalContenedor.classList.remove('modal-active')
+})
 
-      modalCarrito.addEventListener('click', (e) =>{
-          e.stopPropagation();
-      })
+modalContenedor.addEventListener('click', () =>{
+    cerrarCarrito.click();
+})
+
+    modalCarrito.addEventListener('click', (e) =>{
+        e.stopPropagation();
+})
+
+botonVaciarCarrito.addEventListener('click', () => {
+    carritoCompras.length = 0;
+    actualizarCarrito();
+})
