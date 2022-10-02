002ab317a4c18c0716f0ac759be6f601bc2ccd02
Andres Julian Derin <andresderin78@gmail.com>
2022-10-02 19:50:56 -0300

    Ahora si funcionan las cantidades

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ stockProductos.forEach(producto => {
                               <h3>${producto.desc}</h3>
                               <h4>$ ${producto.precio}</h4>
                     </div>
+                    <select name="select" class="card-selectTalles">
+                              <option value="value1">TALLE S</option>
+                              <option value="value2" selected>TALLE M</option>
+                              <option value="value3">TALLE L</option>
+                              <option value="value4">TALLE XL</option>
+                              </select>
                     <div class="card-logo">
                                         <img src="${producto.imgLogo}">
                               <div class="card-logo-texto">
@@ -29,7 +35,12 @@ stockProductos.forEach(producto => {
 
           const btn = document.getElementById(`agregar${producto.id}`)
           btn.addEventListener('click', () => {
-                    agregarAlCarrito(producto.id);
+                    if (validarProductoRepetido(producto)) {
+                              producto.cantidad++;
+                    } else {
+                              agregarAlCarrito(producto.id);
+                    }
                     alert(`Se agrego el producto: ${producto.nombre}.`);
+                    actualizarCarrito();
           });
 });
--- carritoIndex.js
+++ carritoIndex.js
@@ -6,24 +6,26 @@ const precioTotal = document.getElementById('modal-precioTotal');
 
 const actualizarCarrito = () => {
           contenedorCarrito.innerHTML = ""
+          let cantidadCarrito = 0;
           carritoCompras.forEach((producto) => {
                     const div = document.createElement('div');
                     div.classList.add('productoEnCarrito');
 
                     div.innerHTML = `
-                              <img class="carrito-img" src="${producto.img}">
-                                        <div class="class="carrito-contenido">
-                                                  <p>${producto.nombre}</p>
-                                                  <p>Precio: $${producto.precio}</p>
-                                                  <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
-                                                  <button onclick="eliminarDelCarrito(${producto.id})" class="btn-eliminar"><span class="material-symbols-outlined icon-delete">delete</span></button>
-                                        </div>
+                                        <img class="carrito-img" src="${producto.img}">
+                                                  <div class="class="carrito-contenido">
+                                                            <p>${producto.nombre}</p>
+                                                            <p>Precio: $${producto.precio}</p>
+                                                            <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+                                                            <button onclick="eliminarDelCarrito(${producto.id})" class="btn-eliminar"><span class="material-symbols-outlined icon-delete">delete</span></button>
+                                                  </div>
                               </div>
                     `
                     contenedorCarrito.appendChild(div);
+                    cantidadCarrito += producto.cantidad;
           });
 
-          contadorCarrito.innerText = carritoCompras.length;
+          contadorCarrito.innerText = cantidadCarrito;
           precioTotal.innerText = carritoCompras.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
 }
 
@@ -39,4 +41,13 @@ const agregarAlCarrito = (productoId) => {
           carritoCompras.push(item);
           actualizarCarrito();
           console.log(carritoCompras);
-}
+};
+
+const validarProductoRepetido = (p) => {
+          const productoRepetido = carritoCompras.find(producto => producto.id === p.id);
+          if (productoRepetido) {
+              return true;
+          } else {
+              return false;
+          }
+};
