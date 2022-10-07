80a68209d87e9bc6a278cbd034f4a2e2c835449a
Andres Julian Derin <andresderin78@gmail.com>
2022-10-07 00:16:12 -0300

    Guardando todos los cambios que probe en el codigo aparte como talles y filtro de protucos

--- stock.js
+++ stock.js
@@ -1,71 +1,42 @@
-const stockProductos =
+let stockProductos =
 [
-          {
-                    id: 1,
-                    estacion: 'Verano',
-                    nombre: 'Remera',
-                    desc: 'Outfits clásico urbano',
-                    precio: 2500,
-                    img: 'img/remera.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          },
-          {
-                    id: 2,
-                    estacion: 'Verano',
-                    nombre: 'Musculosa',
-                    desc: 'Cancheras estilo urbana/surf',
-                    precio: 2000,
-                    img: 'img/musculosa.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          },
-          {
-                    id: 3,
-                    estacion: 'Verano',
-                    nombre: 'Bermuda',
-                    desc: 'Estilo camuflado combinan cualquier estilo',
-                    precio: 4400,
-                    img: 'img/bermuda.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          },
-          {
-                    id: 4,
-                    estacion: 'Invierno',
-                    nombre: 'Campera',
-                    desc: 'Impermeable, doble polar, capucha y bolsillo interno',
-                    precio: 15000,
-                    img: 'img/camperaDeAbrigo.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          },
-          {
-                    id: 5,
-                    estacion: 'Invierno',
-                    nombre: 'Buzo de polar',
-                    desc: 'Ideal para estación otoño/invierno',
-                    precio: 8000,
-                    img: 'img/buzoDePolar.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          },
-          {
-                    id: 6,
-                    estacion: 'Invierno',
-                    nombre: 'Jogging',
-                    desc: 'Pantalón de frisa cómodo para todos los días',
-                    precio: 5500,
-                    img: 'img/joggingDeFrisa.jpg',
-                    imgLogo: 'img/guilbas logo.png',
-                    slogan: 'Guilbas Urban Wear',
-                    cantidad: 1,
-          }
+{
+id:1,talle:'S',categoria:'Remeras',nombre:'Remera',desc:'Outfits clásico urbano',precio:2500,img:'img/remera.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:2,talle:'M',categoria:'Remeras',nombre:'Remera',desc:'Outfits clásico urbano',precio:2500,img:'img/remera.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:3,talle:'L',categoria:'Remeras',nombre:'Remera',desc:'Outfits clásico urbano',precio:2500,img:'img/remera.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:4,talle:'XL',categoria:'Remeras',nombre:'Remera',desc:'Outfits clásico urbano',precio:2500,img:'img/remera.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:5,talle:'XXL',categoria:'Remeras',nombre:'Remera',desc:'Outfits clásico urbano',precio:2500,img:'img/remera.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+
+
+
+
+
+
+
+{
+id:6,talle:'S',categoria:'Musculosas',nombre:'Musculosa',desc:'Cancheras estilo urbana/surf',precio:2000,img:'img/musculosa.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:7,talle:'S',categoria:'Bermudas',nombre:'Bermuda',desc:'Estilo camuflado combinan cualquier estilo',precio:4400,img:'img/bermuda.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:8,talle:'S',categoria:'Camperas',nombre:'Campera',desc:'Impermeable, doble polar, capucha y bolsillo interno',precio:15000,img:'img/camperaDeAbrigo.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:9,talle:'S',categoria:'Buzos',nombre:'Buzo de polar',desc:'Ideal para estación otoño/invierno',precio:8000,img:'img/buzoDePolar.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+},
+{
+id:10,talle:'S',categoria:'Pantalones',nombre:'Jogging',desc:'Pantalón de frisa cómodo para todos los días',precio:5500,img:'img/joggingDeFrisa.jpg',imgLogo:'img/guilbas logo.png',slogan:'Guilbas Urban Wear',cantidad:1,
+}
 ];
 
 let arrayCategorias = stockProductos;
